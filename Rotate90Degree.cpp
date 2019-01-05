f676318be7edd14ac56f7f2747963ed67c2b6ebc
Andrew Chong <andrew.chong@arm.com>
2019-01-05 20:53:52 +0900

    Finished making the rotation code.

--- Rotate90Degree.cpp
+++ Rotate90Degree.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 /** The dimention of the matrix. */
-#define DIM     3
+#define DIM    9 
 
-void print_matrix(int matrix[DIM][DIM], int dim)
+void print_matrix(int matrix[DIM][DIM])
 {
     for (int i = 0; i < DIM; i++)
     {
@@ -21,9 +21,24 @@ void print_matrix(int matrix[DIM][DIM], int dim)
     }
 }
 
+void rotate_matrix(int input[DIM][DIM], int output[DIM][DIM])
+{
+    for (int y = 0; y < DIM; y++)
+    {
+        for (int x = 0; x < DIM; x++)
+        {
+            int ind = y * DIM + x;
+            int x2 = ind / DIM;
+            int y2 = DIM-1 - (ind % DIM);
+            output[y2][x2] = input[y][x];
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
-    int input[DIM][DIM];
+    int input [DIM][DIM];
+    int output[DIM][DIM];
     int temp = 0;
     for (int i = 0; i < DIM; i++)
     {
@@ -32,8 +47,10 @@ int main(int argc, char** argv)
             input[i][j] = ++temp;
         }
     }
-    print_matrix(input, DIM);
-
-    // TODO: Rotate the input.
+    cout << "Input Matrix:" << endl;
+    print_matrix(input);
+    rotate_matrix(input, output);
+    cout << "Output Matrix:" << endl;
+    print_matrix(output);
 }
 
