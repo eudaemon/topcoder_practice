bfedb37cef4822e4b431b5b3db538251c6a1a7d7
Andrew Chong <sledge76@hotmail.com>
2019-02-02 22:36:26 +0900

    Now at least I am saving data to HashMap.

--- SampleLogParse.cpp
+++ SampleLogParse.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <cstdlib>
 #include <cassert>
+#include <unordered_map> 
 
 using namespace std;
 
@@ -18,6 +19,13 @@ int main () {
         return 0;
     }
 
+    /** HashMap database for storing function name, starting time and
+     * accumulated time usage.
+     * first: starting time. If 'out' is declared, it will be negative.
+     * second: accumulated time of function (out - in). */
+    unordered_map<string, pair<int, int>> umap;
+    unordered_map<string, pair<int, int>>::iterator itr; 
+
     while (getline(myfile,line))
     {
         char * pch;
@@ -31,10 +39,26 @@ int main () {
         if (direction == "in")
         {
             cout << "< " << fname << "," << time << endl;
+            itr = umap.find(fname);
+            if (itr == umap.end())
+            {
+                umap[fname] = make_pair(time,0);
+            }
+            else
+            {
+                assert(itr->second.first < 0);
+                itr->second.first = time;
+            }
         }
         else if (direction == "out")
         {
             cout << "> " << fname << "," << time << endl;
+            itr = umap.find(fname);
+            assert(itr != umap.end());
+            int startT = itr->second.first;
+            assert(startT <= time);
+            itr->second.second += time - startT;
+            itr->second.first = -1;
         }
         else
         {
